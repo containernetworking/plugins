857ec9f90c22f53a6e43f7a7c2183f55118cbefa
Aaron Wood <aaronjwood@gmail.com>
2017-11-02 12:51:54 -0400

    Make sure we propagate any non-zero exit codes up to the caller

--- build.sh
+++ build.sh
@@ -20,12 +20,16 @@ mkdir -p "${PWD}/bin"
 
 echo "Building plugins"
 PLUGINS="plugins/meta/* plugins/main/* plugins/ipam/* plugins/sample"
+ppids=""
 for d in $PLUGINS; do
 	if [ -d "$d" ]; then
 		plugin="$(basename "$d")"
 		echo "  $plugin"
 		go build -o ${PWD}/bin/$plugin -i -pkgdir "$GOPATH/pkg" "$@" "$REPO_PATH/$d" &
+		ppids="$ppids $!"
 	fi
 done
 
-wait
+for ppid in $ppids; do
+	wait $ppid
+done
