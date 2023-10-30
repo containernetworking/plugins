37531cdaf56fec73b8bafa25d0abf3a19f53a7dc
Casey Callendrello <c1@caseyc.net>
2023-10-30 17:55:55 +0100

    fix lint errors
    Fix a small ginkgo compare issue, and ignore dot imports.
    
    Signed-off-by: Casey Callendrello <c1@caseyc.net>

--- pkg/ns/ns_linux_test.go
+++ pkg/ns/ns_linux_test.go
@@ -182,7 +182,7 @@ var _ = Describe("Linux namespace operations", func() {
 				testNsInode, err := getInodeNS(targetNetNS)
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(testNsInode).NotTo(Equal(0))
+				Expect(testNsInode).NotTo(Equal(uint64(0)))
 				Expect(testNsInode).NotTo(Equal(origNSInode))
 			})
 
