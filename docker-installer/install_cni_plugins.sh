04a99b6a30af3038c3f4eb9bc02bfa9ab82a9749
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-12-12 11:56:47 +0000

    Feature: Add release docker image
    Co-authored-by: Michael Wyraz <michael@wyraz.de>
    Signed-off-by: Leonard Jonathan Oh <leonardjonathanoh@live.com>

---
+++ docker-installer/install_cni_plugins.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+set -eu
+
+if [ $# -ne 2 ]; then
+  echo "USAGE: $0 source-dir dest-dir"
+  echo "Env vars:"
+  echo "  FORCE		Overwrite existing binaries"
+  exit 1
+fi
+
+SRC=$1
+DST=$2
+FORCE=${FORCE:-}
+
+install_cni_plugin() {
+	NAME=$1
+	MD5=$( md5sum $SRC/$NAME | awk '{ print $1 }' )
+	if [ -e $DST/$NAME ]; then
+		if [ ! -e $DST/$NAME.by_cni_installer_image ]; then
+			# The file already exists but there's no marker that
+			# it was installed by this installer -> keep untouched
+			if [ -z "$FORCE" ]; then
+				echo "* '$NAME' ignored (exists but not installed by me)"
+				return
+			fi
+		else
+			OTHER_MD5=$( md5sum $DST/$NAME | awk '{ print $1 }' )
+			INSTALLED_MD5=$( cat $DST/$NAME.by_cni_installer_image )
+
+			if [ "$OTHER_MD5" != "$INSTALLED_MD5" ]; then
+				# The file was previously installed by this installer
+				# but later changed -> keep untouched
+				if [ -z "$FORCE" ]; then
+					echo "* '$NAME' ignored (previously installed by me but changed by someone else)"
+					return
+				fi
+			fi
+
+			if [ "$OTHER_MD5" == "$MD5" ]; then
+				# The file was previously installed by this installer
+				# and is up-to-date
+				echo "* '$NAME' is up-to-date"
+				return
+			fi
+		fi
+
+		# The file was previously installed by this installer
+		# but needs an update
+		cp -a $SRC/$NAME $DST/.$NAME.new
+		mv $DST/.$NAME.new $DST/$NAME
+		echo $MD5 > $DST/$NAME.by_cni_installer_image
+		echo "* '$NAME' updated"
+
+	else
+		cp -a $SRC/$NAME $DST/.$NAME.new
+		mv $DST/.$NAME.new $DST/$NAME
+		echo $MD5 > $DST/$NAME.by_cni_installer_image
+		echo "* '$NAME' installed"
+	fi
+}
+
+echo "Installing CNI plug-ins to $DST"
+echo
+
+for FILE in $( find $SRC -maxdepth 1  -type f ); do
+	NAME=$( basename $FILE )
+	install_cni_plugin $NAME
+done
